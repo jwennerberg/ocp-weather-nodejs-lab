b355d210596ab40e3d502863f3ac0291284fa969
Johan Wennerberg <jwennerb@redhat.com>
2016-10-25 12:59:05 +0200

    parameters

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 var app = require('express')();
 var randomstring = require("randomstring");
 var weather = require('openweather-apis');
+var fs = require('fs');
 var urllib = require('urllib');
 const exec = require('child_process').exec;
 
-var appidkey = '08418fc2f148059776aed472e2e417b2';
-
-app.set('port', (process.env.PORT || 8080));
+var appidkey = (process.env.OPENWEATHER_APIKEY || '08418fc2f148059776aed472e2e417b2');
 
 var defaultCity = 'Sigtuna';
+
+var frontendUrl = (process.env.FRONTEND_SERVICE_IP || 'frontend-test-tore.apps.ocp.rocks');
+var ocToken = process.env.OC_TOKEN;
+var ocpApi = process.env.KUBERNETES_SERVICE_HOST;
+var ocpApiPort = process.env.KUBERNETES_SERVICE_PORT_HTTPS;
+var dcScaleUrl = (process.env.DC_TO_SCALE || 'https://' + ocpApi + ':' + ocpApiPort + '/oapi/v1/namespaces/test-tore/deploymentconfigs/frontend/scale');
+
+app.set('port', (process.env.PORT || 8080));
 
 weather.setAPPID(appidkey);
 weather.setLang('en');
@@ -17,32 +24,31 @@ weather.setCity(defaultCity);
 app.get('/api/:nodekey', function(req, res) {
   var nodekey = req.params.nodekey;
 
-  var jsonblob = '{"kind":"Scale","apiVersion":"extensions/v1beta1","metadata":{"name":"frontend","namespace":"test-tore","selfLink":"/oapi/v1/namespaces/test-tore/deploymentconfigs/frontend/scale","uid":"65fcf782-9a8d-11e6-87f8-02af4b58a009","resourceVersion":"106298","creationTimestamp":"2016-10-25T08:31:17Z"},"spec":{ "replicas": 2 } }'
-
-  var fs = require('fs');
-  fs.writeFile("/tmp/json", jsonblob);
+  var jsonblob = {"kind":"Scale","apiVersion":"extensions/v1beta1","metadata":{"name":"frontend","namespace":"test-tore","uid":"65fcf782-9a8d-11e6-87f8-02af4b58a009","resourceVersion":"106298","creationTimestamp":"2016-10-25T08:31:17Z"},"spec":{ "replicas": "2" } };
 
   weather.setCity(nodekey);
 
   weather.getDescription(function(err, desc){
     console.log(desc);
-    //res.send(desc);
     var matches = desc.match(/rain/);
     console.log(matches);
     res.send(matches);
     if (matches !== null) {
-      exec('curl -k -X PUT -H "Authorization: Bearer OrrVzIQ1uusnH8E2TKCp2peeJB7RiZPHVYCjtxqKcIg" -H "Content-Type: application/json" -d @/tmp/json https://openshift-master.ocp.rocks:443/oapi/v1/namespaces/test-tore/deploymentconfigs/frontend/scale', (error, stdout, stderr) => {
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-      });
-      urllib.request('http://frontend-test-tore.apps.ocp.rocks/change.php?weather=w', function (err, data, res) {
+      urllib.request('http://' + frontendUrl + '/change.php?weather=w', function (err, data, res) {
         console.log(data.toString());
       });
+      jsonblob.spec.replicas = "4";
     } else {
-      urllib.request('http://frontend-test-tore.apps.ocp.rocks/change.php?weather=s', function (err, data, res) {
+      urllib.request('http://' + frontendUrl + '/change.php?weather=s', function (err, data, res) {
         console.log(data.toString());
       });
+      jsonblob.spec.replicas = "2";
     }
+    fs.writeFile("/tmp/json", jsonblob);
+    exec('curl -k -X PUT -H "Authorization: Bearer ' + ocToken +'" -H "Content-Type: application/json" -d @/tmp/json ' + dcScaleUrl, (error, stdout, stderr) => {
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+    });
   });
   /*
   weather.getAllWeather(function(err, JSONObj){
