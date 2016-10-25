c61a0d889a73ddbc2ef722bf59697774327d2c35
Johan Wennerberg <jwennerb@redhat.com>
2016-10-25 10:48:30 +0200

    match rain

--- app.js
+++ app.js
@@ -6,35 +6,40 @@ var appidkey = '08418fc2f148059776aed472e2e417b2';
 
 app.set('port', (process.env.PORT || 8080));
 
+var defaultCity = 'Sigtuna';
+
 weather.setAPPID(appidkey);
 weather.setLang('en');
-weather.setCity('Sigtuna');
+weather.setCity(defaultCity);
 
 app.get('/api/:nodekey', function(req, res) {
   var nodekey = req.params.nodekey;
 
   weather.setCity(nodekey);
 
-  /*
   weather.getDescription(function(err, desc){
     console.log(desc);
     res.send(desc);
+    var matches = str.match(/rain/);
+    console.log(matches);
+    res.send(matches);
   });
-  */
+  /*
   weather.getAllWeather(function(err, JSONObj){
     console.log(JSONObj);
     res.send(JSONObj);
   });
-
-  //res.send({ "value": nodekey, "source": JSONObj });
+  */
 
 });
 
 
 app.get('/', function(req, res) {
   weather.getDescription(function(err, desc){
+    console.log(defaultCity);
     console.log(desc);
     res.send(desc);
+    res.send(defaultCity);
   });
 });
 
